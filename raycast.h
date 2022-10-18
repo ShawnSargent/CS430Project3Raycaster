f37352d02624dcf8704128a847c8f156db40089a
Collin <ccr243@nau.edu>
2022-10-17 20:04:51 -0700

    Shelled out .h file
    Collin did this

---
+++ raycast.h
@@ -0,0 +1,16 @@
+// Pre processor Directives
+#ifndef RAYCAST_H
+#define RAYCAST_H
+
+// Header Files
+#include "v3math.h"
+#include "PPMConverterMain.h"
+
+
+// Object Data Structure
+
+
+// Function Prototypes
+
+
+#endif // RAYCAST_H
