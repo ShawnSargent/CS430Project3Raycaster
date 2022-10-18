c2c05395f2c5cdff2e022cf497e19207ccd182bf
Collin <ccr243@nau.edu>
2022-10-17 20:39:30 -0700

    Update raycast.c
    Collin did this

---
+++ raycast.c
@@ -0,0 +1,17 @@
+// Header File
+#include "raycast.h"
+
+// Function: main
+// Purpose: do the desired programming task
+int main( int argc, char **argv )
+{
+
+}
+
+
+// Function: parseInputJSON()
+// Purpose: imports .json file, which is file 
+ObjectListType parseInputJSON( FILE *inputFile )
+{
+
+}
